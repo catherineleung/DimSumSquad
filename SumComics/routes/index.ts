3724b4c399a6b762499150f7f4e39b6f5d673b3c
abmicyau <myau_@live.com>
2016-01-31 15:51:53 -0800

    typescript content added

--- SumComics/routes/index.ts
+++ SumComics/routes/index.ts
@@ -1,62 +1,84 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/>
-var User = (function () {
-    function User(username, email) {
+
+interface UserInterface {
+    getName(): string;
+    getEmail(): string;
+}
+
+class User implements UserInterface {
+    username: string;
+    email: string;
+
+    constructor(username: string, email: string) {
         this.username = username;
         this.email = email;
     }
-    User.prototype.getName = function () {
+
+    getName() {
         return this.username;
-    };
-    User.prototype.getEmail = function () {
+    }
+
+    getEmail() {
         return this.email;
-    };
-    return User;
-})();
-var Router = (function () {
-    function Router() {
+    }
+}
+
+class Router {
+    constructor() {
         var express = require('express');
         var router = express.Router();
+
         /* GET home page. */
-        router.get('/', function (req, res, next) {
-            res.render('index', { title: 'Express' });
+        router.get('/', function(req, res, next) {
+          res.render('index', { title: 'Express' });
         });
+
         /* GET Hello World page */
-        router.get('/helloworld', function (req, res) {
-            res.render('helloworld', { title: 'Hello, World!' });
+        router.get('/helloworld', function(req, res) {
+        	res.render('helloworld', { title: 'Hello, World!' });
         });
+
         /* GET Userlist page. */
-        router.get('/userlist', function (req, res) {
+        router.get('/userlist', function(req, res) {
             var db = req.db;
             var collection = db.get('usercollection');
-            collection.find({}, {}, function (e, docs) {
+            collection.find({},{},function(e,docs){
                 res.render('userlist', {
-                    "userlist": docs
+                    "userlist" : docs
                 });
             });
         });
+
         /* GET New User page. */
-        router.get('/newuser', function (req, res) {
-            res.render('newuser', {
-                title1: 'Add New User',
-                title2: 'Remove User',
-                title3: 'Remove All Users'
+        router.get('/newuser', function(req, res) {
+            res.render('newuser', { 
+            	title1: 'Add New User',
+            	title2: 'Remove User',
+            	title3: 'Remove All Users' 
             });
         });
+
         /* POST to Add User Service */
-        router.post('/adduser', function (req, res) {
+        router.post('/adduser', function(req, res) {
+
             // Set our internal DB variable
             var db = req.db;
+
             // Get our form values. These rely on the "name" attributes
+            
             var user = new User(req.body.username, req.body.useremail);
+
             var userName = user.getName();
             var userEmail = user.getEmail();
+
             // Set our collection
             var collection = db.get('usercollection');
+
             // Submit to the DB
             collection.insert({
-                "username": userName,
-                "email": userEmail
+            	"username" : userName,
+            	"email" : userEmail
             }, function (err, doc) {
                 if (err) {
                     // If it failed, return error
@@ -67,20 +89,30 @@ var Router = (function () {
                     res.redirect("userlist");
                 }
             });
+
+            
         });
+
         // Remove all users
-        router.post('/removeallusers', function (req, res) {
-            var db = req.db;
-            var collection = db.get('usercollection');
-            collection.remove({});
-            res.redirect("userlist");
-        });
+        router.post('/removeallusers', function(req, res) {
+        	var db = req.db;
+
+        	var collection = db.get('usercollection');
+
+        	collection.remove({});
+        	res.redirect("userlist");
+        })
+
         // Remove user
-        router.post('/removeuser', function (req, res) {
-            var db = req.db;
-            var userName = req.body.username;
-            var collection = db.get('usercollection');
-            collection.remove({ "username": { $eq: userName } }, function (err, doc) {
+        router.post('/removeuser', function(req, res) {
+        	var db = req.db;
+
+        	var userName = req.body.username;
+
+        	var collection = db.get('usercollection');
+
+        	collection.remove( { "username" : { $eq: userName } }, 
+        		function (err, doc) {
                 if (err) {
                     // If it failed, return error
                     res.send("There was a problem removing the information to the database.");
@@ -90,9 +122,10 @@ var Router = (function () {
                     res.redirect("userlist");
                 }
             });
-        });
+        })
+
         module.exports = router;
     }
-    return Router;
-})();
+}
+
 var router = new Router();
