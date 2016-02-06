97bd1438ed754501097d9bea9a1af7fa2c9deccf
kayla8l <kaylalee@gmail.com>
2016-02-06 13:51:44 -0800

    file uploading working

--- SumComics/routes/index.js
+++ SumComics/routes/index.js
@@ -1,5 +1,20 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/>
+
+// added for file uploading from: https://codeforgeek.com/2014/11/file-uploads-using-node-js/
+
+var multer  =   require('multer');
+var storage =   multer.diskStorage({
+            destination: function (req, file, callback) {
+            callback(null, './uploads');
+                },
+            filename: function (req, file, callback) {
+            callback(null, file.fieldname + '-' + Date.now());
+            }
+            });
+var upload = multer({ storage : storage}).single('userPhoto');
+// end of adding for file uploading
+
 var Router = (function () {
     function Router() {
         var express = require('express');
@@ -9,6 +24,25 @@ var Router = (function () {
         router.get('/', function (req, res, next) {
             res.render('index', { title: 'Express' });
         });
+
+        /* GET upload page. */
+
+        router.get('/upload',function(req,res){
+            res.sendFile(__dirname + "/upload.html");
+        });
+
+        /* POST/UPLOAD picture */
+
+        router.post('/api/photo',function(req,res){
+            upload(req,res,function(err) {
+            if(err) {
+                return res.end("Error uploading file.");
+            }
+            res.end("File is uploaded");
+            res.redirect('/upload');
+            });
+            });
+
         /* GET Userlist page. */
         router.get('/userlist', function (req, res) {
             var db = req.db;
