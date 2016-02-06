38fc2d230865e892dce43ddcfbc1003b9b252103
Catherine Leung <catherineleung123@gmail.com>
2016-02-06 14:14:16 -0800

    fixed micheal's mistake :^)

--- SumComics/routes/index.js
+++ SumComics/routes/index.js
@@ -1,48 +1,34 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/>
-
-// added for file uploading from: https://codeforgeek.com/2014/11/file-uploads-using-node-js/
-
-var multer  =   require('multer');
-var storage =   multer.diskStorage({
-            destination: function (req, file, callback) {
-            callback(null, './uploads');
-                },
-            filename: function (req, file, callback) {
-            callback(null, file.fieldname + '-' + Date.now());
-            }
-            });
-var upload = multer({ storage : storage}).single('userPhoto');
-// end of adding for file uploading
-
+var User = (function () {
+    function User(username, email, password, confirmpassword) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.confirmpassword = confirmpassword;
+    }
+    User.prototype.getName = function () {
+        return this.username;
+    };
+    User.prototype.getEmail = function () {
+        return this.email;
+    };
+    User.prototype.getPassword = function () {
+        return this.password;
+    };
+    User.prototype.getConfirmPassword = function () {
+        return this.confirmpassword;
+    };
+    return User;
+})();
 var Router = (function () {
     function Router() {
         var express = require('express');
         var router = express.Router();
-        var User = require('../model/user.js');
         /* GET home page. */
         router.get('/', function (req, res, next) {
             res.render('index', { title: 'Express' });
         });
-
-        /* GET upload page. */
-
-        router.get('/upload',function(req,res){
-            res.sendFile(__dirname + "/upload.html");
-        });
-
-        /* POST/UPLOAD picture */
-
-        router.post('/api/photo',function(req,res){
-            upload(req,res,function(err) {
-            if(err) {
-                return res.end("Error uploading file.");
-            }
-            res.end("File is uploaded");
-            res.redirect('/upload');
-            });
-            });
-
         /* GET Userlist page. */
         router.get('/userlist', function (req, res) {
             var db = req.db;
