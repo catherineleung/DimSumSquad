7a646ad205d5148f8d241b4262d3f7c5754bf2b9
abmicyau <myau_@live.com>
2016-02-08 16:55:34 -0800

    added db and auth files

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url': 'mongodb://kathyskrafts:dimsumsquad@ds059185.mongolab.com:59185/heroku_f0bt5b2s' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
