78c345288f95360b2695f3adc0d0efe202fca128
Catherine Leung <catherineleung123@gmail.com>
2016-03-29 13:37:52 -0700

    just shows a ranking of contributors.. not top 6

--- app/routes.js
+++ app/routes.js
@@ -375,6 +375,12 @@ var Router = (function () {
                                 console.log(err);
                         });
 
+                        // decrements user's score by 5
+                        User.findByIdAndUpdate(user._id, { $dec: { 'local.score': 5}}, function (err, data) {
+                            if (err)
+                                console.log(err);
+                        });
+
                         // iterate through the comic's images
                         for (i = 0; i < comic.images.length; i++) {
 
