0e3ad2eb58fed7475b13dc945a6b5a294b5c49af
vyangit <vyan.git@gmail.com>
2016-03-28 20:30:01 -0700

    Added classes for comic chapters

--- app/routes.js
+++ app/routes.js
@@ -156,15 +156,13 @@ var Router = (function () {
             });
 
             // PANEL VIEW ==============================
-            app.get('/comics/:id/panel', function(req, res){
-                Comic.findOne({_id: req.params.id}, function (err, comic) {
-                    User.find({}, function (err, users) {
-                        res.render('panel.ejs', {
-                            user: req.user,
-                            comic: comic
-                        });
+            app.get('/comic/panel/:id', function(req, res){
+                Comic.find({}, function (err, docs) {
+                    res.render('panel.ejs', {
+                        user: req.user,
+                        comics: docs,
                     });
-                });      
+                });         
             });
 
             // ADD COMIC PANEL VIEW ==============================
